dd7f993bf828db0a139ea30c3d707f0184d0ba99
Sijan Ghimire <m@ghimiresijan.com.np>
2024-12-22 11:23:23 +0545

    Header file

---
+++ utilities.h
@@ -0,0 +1,13 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <stdbool.h> // For boolean types
+
+// Function prototypes
+void swap(int *a, int *b);
+void reverseArray(int arr[], int size);
+bool isPrime(int num);
+bool isArmstrong(int num);
+bool isPalindrome(int num);
+
+#endif // UTILITIES_H
