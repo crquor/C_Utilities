b07f54fbf35171f7ad48c8bc0e6c106184992b0f
Sijan Ghimire <m@ghimiresijan.com.np>
2024-12-28 19:26:07 +0545

    Added more functions

--- utilities.h
+++ utilities.h
@@ -2,12 +2,28 @@
 #define UTILITIES_H
 
 #include <stdbool.h> // For boolean types
+#include <math.h>
 
 // Function prototypes
-void swap(int *a, int *b);
-void reverseArray(int arr[], int size);
-bool isPrime(int num);
+
+// Check for different type of numbers:
+
 bool isArmstrong(int num);
+bool isEven(int num);
+bool isOdd(int num);
 bool isPalindrome(int num);
+bool isPrime(int num);
+
+// Operations for integers and integer arrays
+
+int countDigits(int num);
+int factorial(int num);
+int maxInArray(int arr[], int size);
+int minInArray(int arr[], int size);
+void reverseArray(int arr[], int size);
+void rotateArray(int arr[], int size, int key);
+void swap(int *a, int *b);
+
+// Operations for strings
 
 #endif // UTILITIES_H
